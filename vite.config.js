dc3ee7506d9eedc6d89245dcce803e9a4d435280
Manuel Zuniga <manuzudev@gmail.com>
2024-07-08 10:58:02 -0600

    Add files via upload
    Adding files via upload...

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,21 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { fileURLToPath } from "url";
+import { defineConfig } from "vite";
+import { createVuePlugin as vue2 } from "vite-plugin-vue2";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
-})
+  plugins: [
+    vue2({
+      jsx: true,
+    }),
+  ],
+  resolve: {
+    alias: {
+      "@": fileURLToPath(new URL("./src", import.meta.url)),
+      vue: "vue/dist/vue.esm.js",
+    },
+  },
+  build: {
+    brotliSize: false, // unsupported in StackBlitz
+  },
+});
